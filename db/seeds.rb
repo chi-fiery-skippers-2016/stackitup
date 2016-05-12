9867f20007838bfc8384242fdb95bc22028ca2dc
Dexter Moran & Lisa Dannewitz <pair+dextermoran.ldannewitz@devbootcamp.com>
2016-05-11 21:26:50 -0500

    add non functing rspec -- probably something wrong with associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ end
 15.times do
   vote = Vote.create!(
     voteable_type: rand(2) == 1 ? 'question' : 'answer',
-    voteable_id: 1 + rand(9),
+    voteable_id: rand(10) + 1,
     voter_id: rand(3) + 1,
     up?: rand(2) == 1 ? true : false
     )
@@ -26,7 +26,7 @@ end
   response = Response.create!(
     body: Faker::StarWars.quote,
     respondable_type: rand(2) == 1 ? 'question' : 'answer',
-    respondable_id: rand(9) + 1,
+    respondable_id: rand(10) + 1,
     user_id: rand(3) + 1
     )
 end
@@ -34,30 +34,26 @@ end
 10.times do
   answer = Answer.create!(
     body: Faker::Lorem.sentence,
-    question_id: 1 + rand(4),
-    the_one_who_answers_id: 1 + rand(2),
+    question_id: rand(10) + 1,
+    theOneWhoAnswers_id: rand(3) + 1,
     best?: false
     )
 end
 
-
 ## CAN I COMMENT OR ANSWER ON MY OWN POST??
-
-
-
 
 10.times do
   question = Question.create!(
     title: Faker::StarWars.quote,
     body: Faker::Hipster.paragraph,
-    author_id: 1 + rand(2),
+    author_id: rand(3) + 1,
     view_count: rand(1000)
     )
 end
 
 20.times do
   tagging = Tagging.create!(
-     tag_id: 1 + rand(19),
-     question_id: 1 + rand(9)
+     tag_id: rand(20) + 1,
+     question_id: rand(10) + 1
     )
 end
--- spec/models/models_spec.rb
+++ spec/models/models_spec.rb
@@ -4,22 +4,22 @@ require_relative '../../app/models/answer'
 require_relative '../../app/models/user'
 require_relative '../../app/models/response'
 
-describe 'models' do
-  let(:question) {Question.new(title: 'To be or not to be', body: 'That is the question', author_id: 1, view_count: 13)}
+describe 'models-' do
+  let(:question) {Question.create!(title: 'To be or not to be', body: 'That is the question', author_id: 1, view_count: 13)}
 
-  let(:author) {User.new(username: 'Darth Vader', email: 'blowyourdeathstar@hotmail.com', hashword: 'passwordPasswordPassword')}
+  let(:author) {User.create!(username: 'Darth Vader', email: 'blowyourdeathstar@hotmail.com', hashword: 'passwordPasswordPassword')}
 
-  let(:commenter) {User.new(username: 'old man', email: 'email@email.com', hashword: 'password')}
+  let(:commenter) {User.create!(username: 'old man', email: 'email@email.com', hashword: 'password')}
 
-  let(:answer) {Answer.new(body: 'your shit sucks', question_id: 1, the_one_who_answers_id: 2, best?: false)}
+  let(:answer) {Answer.create!(body: 'your shit sucks', question_id: 1, the_one_who_answers_id: 2, best?: false)}
 
-  let(:response) {Response.new(body: 'this is a Star Wars quote', respondable_id: 1,
+  let(:response) {Response.create!(body: 'this is a Star Wars quote', respondable_id: 1,
       respondable_type: 'question', user_id: 1
       )}
 
-  let(:vote) {Vote.new(voteable_id: 1, voteable_type: 'question', voter_id: 2, up?: true)}
+  let(:vote) {Vote.create!(voteable_id: 1, voteable_type: 'question', voter_id: 2, up?: true)}
 
-  let(:tag) {Tag.new(name: 'bahaha')}
+  let(:tag) {Tag.create!(name: 'bahaha')}
 
   describe Question do
 
@@ -28,7 +28,7 @@ describe 'models' do
     end
 
     it 'has votes' do
-      expect(question.votes[0].class).to be(Vote)
+      expect(question.votes[0]).to be_a(Vote)
     end
   end
 end
