793ac24c1b7ab1d5450ef90c45a434cc7f5bdb4c
Dexter Moran & Lisa Dannewitz <pair+dextermoran.ldannewitz@devbootcamp.com>
2016-05-12 09:38:18 -0500

    PASSING RSPEC

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :questions
-  has_many :answers
+  has_many :questions, foreign_key: :author_id
+  has_many :answers, foreign_key: :theOneWhoAnswers_id
   has_many :responses
-  has_many :votes
+  has_many :votes, foreign_key: :voter_id
 end
