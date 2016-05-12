0fb90d35e0bc051151a0d7249ece145926e0231a
[Frank Lam] <[ryzingsun11@yahoo.com]>
2016-05-12 12:48:36 +0000

    Update last_asked helper

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,3 +1,14 @@
-def last_answered(time)
-  ((Time.now - time)/3600).round
+def last_asked(time)
+  hours = ((Time.now - time)/3600).round
+  minutes = ((Time.now - time)/60).round
+  
+  if hours > 24
+    (hours/24).round
+  end
+  
+  if minutes < 1 #days
+    (minutes*60).round #econds
+  else
+    minutes
+  end
 end
