828affc7b9cf6a5ef127f1eb42e40634696dad54
dextermoran <dextermoran@gmail.com>
2016-05-12 19:23:51 -0500

    add a bunch of styling on buttons

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -14,3 +14,21 @@ def last_asked(time)
     return "asked: #{minutes} minute(s) ago"
   end
 end
+
+
+def last_time(time)
+  hours = ((Time.now - time)/3600).round
+  minutes = ((Time.now - time)/60).round
+
+  if hours > 24
+    return "#{(hours/24).round} day(s) ago"
+  end
+
+  if minutes < 1
+    return "#{(minutes*60).round} seconds ago"
+  elsif minutes >=60
+    return "#{hours} hour(s) ago"
+  else
+    return "#{minutes} minute(s) ago"
+  end
+end
